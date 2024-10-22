79b2103f9cf15347b495bbed69baa44b0e1db65f
M00St4r <bjoern.martens@posteo.de>
2024-10-22 11:37:38 +0200

    add canon interface

--- Ballerburgbjch/Ballerburg.ts
+++ Ballerburgbjch/Ballerburg.ts
@@ -1,9 +1,24 @@
 window.addEventListener("load", _event => {
 
+    interface canon {
+
+        direction: {
+            x: number,
+            y: number
+        },
+
+        position: {
+            x: number,
+            y: number
+        },
+
+        power: number
+    }
+
     function animate() {
-        
+
         requestAnimationFrame(animate);
     }
-    
+
     requestAnimationFrame(animate);
 });
