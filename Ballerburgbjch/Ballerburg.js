622a44e28a9daa20c96b5122dbe8f8437b87dd4c
M00St4r <bjoern.martens@posteo.de>
2024-10-22 11:58:20 +0200

    draw background

--- Ballerburgbjch/Ballerburg.js
+++ Ballerburgbjch/Ballerburg.js
@@ -1,1 +1,23 @@
 "use strict";
+window.addEventListener("load", _event => {
+    const canvas = document.getElementsByTagName("canvas")[0];
+    const ctx = canvas.getContext("2d");
+    let canvasX = canvas.width;
+    let canvasY = canvas.height;
+    let worldWidth = 1920;
+    let worldHeigth = 1080;
+    let worldPosX = 0;
+    let worldPosY = 0;
+    function drawBackground() {
+        //draw background
+        ctx.fillStyle = "rgb(77, 57, 20)";
+        let bg = new Path2D();
+        bg.rect(worldPosX, worldPosY, worldWidth, worldHeigth);
+        ctx.fill(bg);
+    }
+    function animate() {
+        drawBackground();
+        requestAnimationFrame(animate);
+    }
+    requestAnimationFrame(animate);
+});
--- Ballerburgbjch/Ballerburg.ts
+++ Ballerburgbjch/Ballerburg.ts
@@ -39,8 +39,26 @@ window.addEventListener("load", _event => {
         yPos: number
     }
 
-    function animate() {
+    const canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
+    const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!;
 
+    let canvasX: number = canvas.width;
+    let canvasY: number = canvas.height;
+    let worldWidth: number = 1920;
+    let worldHeigth: number = 1080;
+    let worldPosX: number = 0;
+    let worldPosY: number = 0;
+
+    function drawBackground() {
+        //draw background
+        ctx.fillStyle = "rgb(77, 57, 20)";
+        let bg: Path2D = new Path2D();
+        bg.rect(worldPosX, worldPosY, worldWidth, worldHeigth);
+        ctx.fill(bg);
+    }
+
+    function animate() {
+        drawBackground();
         requestAnimationFrame(animate);
     }
 
